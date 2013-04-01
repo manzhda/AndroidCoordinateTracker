fa902773ba6dd16f5fca8b3e965c95cf2e8d48fb
Dmitriy Manzhosov <manzhda@gmail.com>
2013-04-01 13:00:16 +0300

    Reconstract project structure. Now in root library and sample folders

--- src/com/mda/coordinatetracker/CoordinateReceiver.java
+++
@@ -1,9 +0,0 @@
-package com.mda.coordinatetracker;
-
-/**
- * User: mda
- * Date: 3/29/13
- * Time: 5:18 PM
- */
-public class CoordinateReceiver {
-}
--- src/com/mda/coordinatetracker/network/Connection.java
+++
@@ -1,9 +0,0 @@
-package com.mda.coordinatetracker.network;
-
-/**
- * User: mda
- * Date: 4/1/13
- * Time: 11:35 AM
- */
-public class Connection {
-}
