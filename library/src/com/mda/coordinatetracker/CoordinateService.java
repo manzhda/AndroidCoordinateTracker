198a91a855c00c1d58bfbc65d31b17d93849543e
Dmitriy Manzhosov <manzhda@gmail.com>
2013-04-03 15:50:29 +0300

    Added different filter for Broadcast Receivers. Made send broadcast via LocalBroadcastManager.

--- library/src/com/mda/coordinatetracker/CoordinateService.java
+++ library/src/com/mda/coordinatetracker/CoordinateService.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
 import android.os.Parcelable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.widget.Toast;
 import com.mda.coordinatetracker.geocoder.GeocoderAsyncTask;
 import com.mda.coordinatetracker.geocoder.dto.Location;
@@ -20,13 +21,48 @@ import com.mda.coordinatetracker.receivers.LocationByAddressReceiver;
  * Time: 2:16 AM
  */
 public class CoordinateService extends Service {
+
+    public static void requestCurrentLocation(Context context, CurrentLocationReceiver currentLocationReceiver){
+        register(context, currentLocationReceiver, FILTER_ACTION_CURRENT_LOCATION);
+        Intent intent = createIntent(context, CoordinateService.ACTION_GET_CURRENT_LOCATION);
+        context.startService(intent);
+    }
+
+    public static void requestLocation(Context context, LocationByAddressReceiver locationByAddressReceiver, String address){
+        register(context, locationByAddressReceiver, FILTER_ACTION_DIRECT_GEOCODE);
+        Intent intent = createIntent(context, CoordinateService.ACTION_DIRECT_GEOCODE);
+        intent.putExtra(PARAM_REQUESTED_ADDRESS, address);
+        context.startService(intent);
+    }
+
+    public static void startTrackAddress(Context context, AddressReceiver coordinateReceiver){
+        register(context, coordinateReceiver, FILTER_ACTION_TRACK_LOCATION);
+        Intent intent = createIntent(context, CoordinateService.ACTION_START);
+        context.startService(intent);
+    }
+
+    public static void stopTrackAddress(Context context, BroadcastReceiver receiver){
+        try {
+            LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(context.getApplicationContext());
+            broadcastManager.unregisterReceiver(receiver);
+        } catch (IllegalArgumentException ex){
+            Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        Intent intent = createIntent(context, CoordinateService.ACTION_STOP);
+        context.startService(intent);
+    }
+
+
+
     public static final String ACTION_START = "com.mda.coordinatetracker.START";
     public static final String ACTION_STOP = "com.mda.coordinatetracker.STOP";
 
     public static final String ACTION_DIRECT_GEOCODE = "com.mda.coordinatetracker.ACTION_DIRECT_GEOCODE";
     public static final String ACTION_GET_CURRENT_LOCATION = "com.mda.coordinatetracker.ACTION_GET_CURRENT_LOCATION";
 
-    public static final String FILTER_ACTION = "com.mda.coordinatetracker.FILTER_ACTION";
+    public static final String FILTER_ACTION_CURRENT_LOCATION = "com.mda.coordinatetracker.FILTER_ACTION_CURRENT_LOCATION";
+    public static final String FILTER_ACTION_DIRECT_GEOCODE = "com.mda.coordinatetracker.FILTER_ACTION_DIRECT_GEOCODE";
+    public static final String FILTER_ACTION_TRACK_LOCATION = "com.mda.coordinatetracker.FILTER_ACTION_TRACK_LOCATION";
 
     public static final String PARAM_STATUS = "com.mda.coordinatetracker.PARAM_STATUS";
     public static final String PARAM_RESULT = "com.mda.coordinatetracker.PARAM_RESULT";
@@ -55,32 +91,19 @@ public class CoordinateService extends Service {
 
             @Override
             public void onLocationRetrieved(android.location.Location location, CoordinateManager coordinateManager) {
-                onResult(STATUS_LOCATION_RETRIEVED, location);
+                onResult(STATUS_LOCATION_RETRIEVED, FILTER_ACTION_CURRENT_LOCATION, location);
             }
 
             @Override
             public void onLocationError(CoordinateManager coordinateManager) {
-                onResult(STATUS_LOCATION_ERROR);
+                onResult(STATUS_LOCATION_ERROR, FILTER_ACTION_CURRENT_LOCATION);
             }
 
             @Override
             public void onNetworkError(CoordinateManager coordinateManager) {
-                onResult(STATUS_NETWORK_ERROR);
+                onResult(STATUS_NETWORK_ERROR, FILTER_ACTION_CURRENT_LOCATION);
             }
         });
-    }
-
-    private void onResult(int status){
-        Intent intent = new Intent(FILTER_ACTION);
-        intent.putExtra(PARAM_STATUS, status);
-        sendBroadcast(intent);
-    }
-
-    private void onResult(int status, Parcelable result){
-        Intent intent = new Intent(FILTER_ACTION);
-        intent.putExtra(PARAM_STATUS, status);
-        intent.putExtra(PARAM_RESULT, result);
-        sendBroadcast(intent);
     }
 
 
@@ -100,7 +123,7 @@ public class CoordinateService extends Service {
                 @Override
                 protected void onPostExecute(Location location) {
                     super.onPostExecute(location);
-                    onResult(STATUS_LOCATION_RETRIEVED, location);
+                    onResult(STATUS_LOCATION_RETRIEVED, FILTER_ACTION_DIRECT_GEOCODE, location);
                 }
             }.execute(address);
         } else if (action.equals(ACTION_GET_CURRENT_LOCATION)) {
@@ -110,38 +133,11 @@ public class CoordinateService extends Service {
         return START_NOT_STICKY;
     }
 
-    public static void requestCurrentLocation(Context context, CurrentLocationReceiver currentLocationReceiver){
-        register(context, currentLocationReceiver);
-        Intent intent = createIntent(context, CoordinateService.ACTION_GET_CURRENT_LOCATION);
-        context.startService(intent);
-    }
 
-    public static void requestLocation(Context context, LocationByAddressReceiver locationByAddressReceiver, String address){
-        register(context, locationByAddressReceiver);
-        Intent intent = createIntent(context, CoordinateService.ACTION_DIRECT_GEOCODE);
-        intent.putExtra(PARAM_REQUESTED_ADDRESS, address);
-        context.startService(intent);
-    }
-
-    public static void startTrackAddress(Context context, AddressReceiver coordinateReceiver){
-        register(context, coordinateReceiver);
-        Intent intent = createIntent(context, CoordinateService.ACTION_START);
-        context.startService(intent);
-    }
-
-    public static void stopTrackAddress(Context context, BroadcastReceiver receiver){
-        try {
-            context.unregisterReceiver(receiver);
-        } catch (IllegalArgumentException ex){
-            Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
-        }
-        Intent intent = createIntent(context, CoordinateService.ACTION_STOP);
-        context.startService(intent);
-    }
-
-    private static void register(Context context, BroadcastReceiver broadcast){
-        IntentFilter locationIntentFilter = new IntentFilter(CoordinateService.FILTER_ACTION);
-        context.registerReceiver(broadcast, locationIntentFilter);
+    private static void register(Context context, BroadcastReceiver broadcast, String action){
+        IntentFilter locationIntentFilter = new IntentFilter(action);
+        LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(context.getApplicationContext());
+        broadcastManager.registerReceiver(broadcast, locationIntentFilter);
     }
 
     private static Intent createIntent(Context context, String action){
@@ -149,5 +145,23 @@ public class CoordinateService extends Service {
         intent.setAction(action);
         intent.setPackage(context.getPackageName());
         return intent;
+    }
+
+    private void onResult(int status, String action){
+        Intent intent = new Intent(action);
+        intent.putExtra(PARAM_STATUS, status);
+        sendLocalBroadcast(intent);
+    }
+
+    private void onResult(int status, String action, Parcelable result){
+        Intent intent = new Intent(action);
+        intent.putExtra(PARAM_STATUS, status);
+        intent.putExtra(PARAM_RESULT, result);
+        sendLocalBroadcast(intent);
+    }
+
+    private void sendLocalBroadcast(Intent intent){
+        LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(getApplicationContext());
+        broadcastManager.sendBroadcast(intent);
     }
 }
